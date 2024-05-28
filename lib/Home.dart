d53525f81da1a7478345838134a8b12b2b41d90e
Robério <rroberiosouza@gmail.com>
2024-05-28 11:25:16 -0300

    Implementado tela de home com imagens, text e botão de ação. Implementado função async que consulta o preço do bitcoin

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,10 +11,48 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double _preco = 0;
+  _consultarBitcoin() async {
+    var url = Uri.parse("https://blockchain.info/ticker");
+    http.Response respose = await http.get(url);
+    Map<String, dynamic> data = json.decode(respose.body);
+
+    setState(() {
+      _preco = data["BRL"]["buy"] as double;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(30),
+            child: Image.asset("images/bitcoin.png"),
+          ),
+          Text(
+            "R\$ ${_preco.toString()}",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: ElevatedButton(
+              onPressed: _consultarBitcoin,
+              child: Text(
+                "Consultar",
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.orange)),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:preco_bitcoin/Home.dart';
 
 void main() {
   runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Home(),
   ));
 }
